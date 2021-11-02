2b3096b173c168748401b9adba44ed31727bb4b4
Firrez <246299@via.dk>
2021-11-02 19:45:46 +0100

    Tier2 addUser oprettet og virker!

---
+++ Tier2-BusinessAPI/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tier2_BusinessAPI.Models;
+
+namespace Tier2_BusinessAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public async Task AddUserAsync(User user)
+        {
+            using HttpClient client = new HttpClient();
+
+            string UserAsJson = JsonSerializer.Serialize(user, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+            HttpContent content = new StringContent(UserAsJson, Encoding.UTF8, "application/json");
+            HttpResponseMessage responseMessage =
+                await client.PostAsync("http://localhost:8080/user", content);
+        }
+    }
+}
---
+++ Tier2-BusinessAPI/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Tier2_BusinessAPI.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        //public int UserId { get; set; }
+    }
+}
