ccb58f897457cdbb554f114ec42418f2ec890c3e
Firrez <246299@via.dk>
2021-11-03 08:19:39 +0100

    Tier 2 get all users

--- Tier2-BusinessAPI/Controllers/UserController.cs
+++ Tier2-BusinessAPI/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
@@ -24,6 +25,27 @@ namespace Tier2_BusinessAPI.Controllers
             HttpContent content = new StringContent(UserAsJson, Encoding.UTF8, "application/json");
             HttpResponseMessage responseMessage =
                 await client.PostAsync("http://localhost:8080/user", content);
+        }
+
+        [HttpGet]
+        public async Task<List<User>> GetUsersAsync()
+        {
+            using HttpClient client = new HttpClient();
+            HttpResponseMessage message = await client.GetAsync("http://localhost:8080/users");
+
+            if (!message.IsSuccessStatusCode)
+            {
+                throw new Exception($@"Error: {message.StatusCode}");
+            }
+
+            string result = await message.Content.ReadAsStringAsync();
+
+            List<User> users = JsonSerializer.Deserialize<List<User>>(result, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+
+            return users;
         }
     }
 }
